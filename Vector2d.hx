452334f3079b73314fbc660cc6bed9653c23c231
RobRas <bobbyras90@gmail.com>
2015-01-14 06:57:12 -0700

    Constructor
    Made the constructor and the x, y variables.

---
+++ Vector2d.hx
@@ -0,0 +1,14 @@
+class Vector2d {
+	var x : Float;
+	var y : Float;
+	
+	public function new() {
+		x = 0;
+		y = 0;
+	}
+	
+	public function new(x:Float, y:Float) {
+		this.x = x;
+		this.y = y;
+	}
+}
